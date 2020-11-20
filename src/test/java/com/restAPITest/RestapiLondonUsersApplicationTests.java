456c2b92373eca526c61438c09def03d1c117897
seemabansode <seems.bansode@gmail.com>
2020-11-20 19:14:14 +0000

    First Commit

---
+++ src/test/java/com/restAPITest/RestapiLondonUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restAPITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiLondonUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
